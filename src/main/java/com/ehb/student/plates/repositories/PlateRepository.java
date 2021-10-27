956a748f24972a6134b1f683ddb3f799d03cc7d1
Tanguy Deleu <tanguyndd@gmail.com>
2021-10-27 20:35:29 +0200

    Moved backend project under backend folder

--- src/main/java/com/ehb/student/plates/repositories/PlateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ehb.student.plates.repositories;
-
-import com.ehb.student.plates.entities.Plate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlateRepository extends JpaRepository<Plate, Long> {
-}
