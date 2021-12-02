398911f8657bc966997b920c063fc835d2ef6280
YoniCelen <yoni.celen@hotmail.com>
2021-12-02 19:35:12 +0100

    added CrossOrigin to PlateController and UserController

--- backend/src/main/java/com/ehb/student/plates/web/controllers/UserController.java
+++ backend/src/main/java/com/ehb/student/plates/web/controllers/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 public class UserController {
 
