1b9c1a8b3fd0e8496b64aeb69df526bba9cae5d9
YoniCelen <yoni.celen@hotmail.com>
2021-12-02 05:06:06 +0100

    forgot dependency

--- backend/src/main/java/com/ehb/student/plates/services/user/UserServiceImpl.java
+++ backend/src/main/java/com/ehb/student/plates/services/user/UserServiceImpl.java
@@ -2,12 +2,14 @@ package com.ehb.student.plates.services.user;
 
 import com.ehb.student.plates.entities.User;
 import com.ehb.student.plates.repositories.UserRepository;
+import com.ehb.student.plates.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
 
 import java.security.Principal;
 
