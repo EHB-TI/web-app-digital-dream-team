d2efee5e988f7565032ec5d46e0c405a92bbfe16
YoniCelen <yoni.celen@hotmail.com>
2021-12-03 17:00:37 +0100

    removed duplicate

--- backend/src/main/java/com/ehb/student/plates/filters/auth/AuthenticationFilter.java
+++ backend/src/main/java/com/ehb/student/plates/filters/auth/AuthenticationFilter.java
@@ -45,18 +45,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) {
-        User user = (User) auth.getPrincipal();
-        String token = Jwts.builder()
-                .setSubject(user.getUsername())
-                .setExpiration(new Date(System.currentTimeMillis() + 1_800_000))
-                .signWith(SignatureAlgorithm.HS512, "PlatesSecretKey".getBytes())
-                .compact();
-
-        response.addHeader("Authorization", "Bearer " + token);
-    }
-
-    @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) throws IOException {
         User user = (User) auth.getPrincipal();
         String token = Jwts.builder()
